5d0ca01cf2104d40c6a2b140a22e6c5e75dc34f0
pedrokpdo <pedromotoehm@gmail.com>
2022-05-13 11:21:33 -0300

    terminando header

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,21 +1,24 @@
 import { Button } from "@mui/material"
 import { AppBar } from '@mui/material'
 import { Toolbar } from "@mui/material"
-
+import { SvgIcon } from "@mui/material"
+import { Bell } from "react-feather"
+import { Avatar } from "@mui/material"
 
 export const Header = () => {
     return (
-        <AppBar>
-                <Toolbar className="toolbar">
-                    <div className="">
-                        <span>Conecta-Dev</span>
-                    </div>
-                    <div className="">
-                        <Button color='primary' variant="contained">Novo Post</Button>
-                        <span>img1</span>
-                        <span>img2</span>
-                    </div>
-                </Toolbar>
-            </AppBar>
+        <AppBar style={{ boxShadow: 'none' }} position="fixed" color='inherit'>
+            <Toolbar className="toolbar">
+                <h1>Conecta-Dev</h1>
+                <div style={{flexGrow:'1'}}></div>
+                <div style={{display:'flex', alignItens:'center'}}>
+                    <Button style={{marginRight:'10px'}} color='primary' variant="contained">Novo Post</Button>
+                    <SvgIcon>
+                        <Bell/>
+                    </SvgIcon>
+                    <Avatar style={{marginLeft:'10px'}}/>
+                </div>
+            </Toolbar>
+        </AppBar>
     )
 }
